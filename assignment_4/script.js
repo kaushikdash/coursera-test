db2138533fd83c5e412a9d10a82563c8c1ad6b25
Dash <kaushik.dash@honeywell.com>
2018-09-21 16:19:10 -0700

    assignment4

--- assignment_4/script.js
+++ assignment_4/script.js
@@ -61,7 +61,7 @@ for (var iterator = 0; iterator < names.length; iterator++) {
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  if (firstLetter === "j") {
+  if (firstLetter === 'j') {
      byeSpeaker.speak(names[iterator]);
   } else {
      helloSpeaker.speak(names[iterator]);
